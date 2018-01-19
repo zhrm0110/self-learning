826836c3a0d7ac2fb4c6d4b244d6225146f42c4e
Rina Zhao <rina.zhao@sap.com>
2018-01-19 16:54:32 +0800

    test code

--- src/spring/cloud/loadbalance/robbion-loadbaiance/src/main/java/com/rina/exercise/RobbionLoadbaianceApplication.java
+++ src/spring/cloud/loadbalance/robbion-loadbaiance/src/main/java/com/rina/exercise/RobbionLoadbaianceApplication.java
@@ -1,23 +1,55 @@
 package com.rina.exercise;
 
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class RobbionLoadbaianceApplication {
 
     @Bean
-    @LoadBalanced
+    // @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
     public static void main(String[] args) {
+        // 多线程测试代码,测试luckygame。
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 300; i++) {
+            final int index = i;
+            cachedThreadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    RestTemplate template = new RestTemplate();
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.set("Authorization", "Basic cmluYTpyaW5h");
+                    HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+                    String openId = UUID.randomUUID().toString();
+                    String root = "https://dis.xixilz.com/luckygame/players/";
+                    String rootLocal = "http://localhost:8080/players/";
+                    String url = rootLocal + openId + "/gameOver";
+                    template.exchange(url, HttpMethod.GET, entity, String.class);
+                    String url2 = rootLocal + openId + "/predefinedMascot?mascot=盆景";
+                    template.exchange(url2, HttpMethod.GET, entity, String.class);
+                    String url4 = rootLocal + openId + "/predefinedMascot?mascot=杯子";
+                    template.exchange(url4, HttpMethod.GET, entity, String.class);
+                    String url5 = rootLocal + openId + "/predefinedMascot?mascot=女孩";
+                    template.exchange(url5, HttpMethod.GET, entity, String.class);
+                    String url3 = rootLocal + openId + "/qrCode";
+                    template.exchange(url3, HttpMethod.GET, entity, String.class);
+                }
+            });
+        }
         SpringApplication.run(RobbionLoadbaianceApplication.class, args);
     }
 }
--- src/spring/cloud/loadbalance/robbion-loadbaiance/src/main/java/com/rina/exercise/controller/ConsumerController.java
+++ src/spring/cloud/loadbalance/robbion-loadbaiance/src/main/java/com/rina/exercise/controller/ConsumerController.java
@@ -1,5 +1,10 @@
 package com.rina.exercise.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Locale;
 
@@ -14,6 +19,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class ConsumerController {
@@ -32,19 +41,73 @@ public class ConsumerController {
     }
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam String lang) {
+    public String hello(
+            // @RequestParam String lang
+            ) {
         /**
          * Here please note that: In case encounter "No instances available for COMPUTE-SERVICE" error, switch of your
          * fire wall and try to access the consumer /add api again.
          */
         // return restTemplate.getForEntity(, String.class).getBody();
         return restTemplate.exchange(
-                UriComponentsBuilder.fromHttpUrl("http://compute-service/hello").build().encode().toUri(),
-                // UriComponentsBuilder.fromHttpUrl("https://computelocale.cfapps.us10.hana.ondemand.com/hello").build().encode().toUri(),
-                // UriComponentsBuilder.fromHttpUrl("https://api.beta.yaas.io/testeventgame/compute/v1/hello").build().encode().toUri(),
-                HttpMethod.GET, getRequestEntity(null, lang), String.class)
+                // UriComponentsBuilder.fromHttpUrl("http://compute-service/hello").build().encode().toUri(),
+                UriComponentsBuilder.fromHttpUrl("http://localhost:2223/hello").build().encode().toUri(),
+                // UriComponentsBuilder.fromHttpUrl("https://computelocale.com/hello").build().encode().toUri(),
+                // UriComponentsBuilder.fromHttpUrl("https://api.io/testeventgame/compute/v1/hello").build().encode().toUri(),
+                // HttpMethod.GET, getRequestEntity(null, lang), String.class)
+                HttpMethod.GET, null, String.class)
                 .getBody();
 
+    }
+
+    @RequestMapping(value = "/testbc", method = RequestMethod.GET)
+    public void testbc(@RequestParam String lang) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Object marketConfig;
+        try {
+            marketConfig = objectMapper.readValue(
+                    new File(getFileURI("/market.json")), Object.class);
+            restTemplate.exchange(
+                    UriComponentsBuilder.fromHttpUrl("http://127.0.0.1:8080/s2s/v1/config/RinaTest/Market/v1").build()
+                            .encode().toUri(),
+                    // UriComponentsBuilder.fromHttpUrl("https://computelocale.com/hello").build().encode().toUri(),
+                    // UriComponentsBuilder.fromHttpUrl("https://api.io/testeventgame/compute/v1/hello").build().encode().toUri(),
+                    HttpMethod.PUT, putRequestEntity(marketConfig, lang), String.class)
+                    .getBody();
+        } catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    private HttpEntity<?> putRequestEntity(Object marketConfig, String lang) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept-Language", new Locale(lang).toString());
+        headers.set("Authorization", "Basic ****");
+        headers.set("hybris-tenant", "rinatest");
+        headers.set("hybris-user", "rina");
+
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        return new HttpEntity<>(marketConfig, headers);
+    }
+
+    private URI getFileURI(String path) {
+        final URL ruleURL = ConsumerController.class.getResource(path);
+        try {
+            return ruleURL.toURI();
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private HttpEntity<?> getRequestEntity(Object payload, String lang) {
